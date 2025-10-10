ec941371a3e5ca8e084e64cc2486163ffc184fda
webdeveloper3188 <42509028+webdeveloper3188@users.noreply.github.com>
2025-10-10 23:11:17 +0530

    Added  the JenkisnFile and Dockerfile

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post("/upload", upload.single("file"), (req, res) => {
 });
 
 // Handle all frontend routes
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../web/index.html"));
 });
 
